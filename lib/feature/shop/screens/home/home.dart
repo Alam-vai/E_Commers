4a44eaaab61f57ad0c9090cf2a8bf2bd9b358576
alam234970 <mdmasulam2349@gmail.com>
2025-07-28 04:15:19 +0600

    Home Search Bar Bar UI

--- lib/feature/shop/screens/home/home.dart
+++ lib/feature/shop/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:socialclone/comon/widgets/products/cart/cart_counter_icon.dart';
+import 'package:socialclone/comon/widgets/textfields/search_bar.dart';
 import 'package:socialclone/feature/shop/screens/home/widgets/home_appbar.dart';
 import 'package:socialclone/feature/shop/screens/home/widgets/primary_header_container.dart';
 import 'package:socialclone/utils/constants/colors.dart';
@@ -17,16 +18,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: UPrimaryHeaderContainer(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            UHomeAppbar(context),
-          ],
-        ),
+      body: Stack(
+        children: [
+          SizedBox(height: USizes.profilePrimaryHeaderHeight * 2),
+
+
+          UPrimaryHeaderContainer(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [UHomeAppbar(context)],
+            ),
+          ),
+          // Search Bar
+
+          USearchBar()
+
+        ],
       ),
     );
   }
-
-
 }
