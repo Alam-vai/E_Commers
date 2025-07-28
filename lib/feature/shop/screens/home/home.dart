a80ad99e96e3cf0241e29ec7c4eff38367000e58
mdmas <mdmasudalam2349@gmail.com>
2025-07-28 20:36:01 +0600

    12.43

--- lib/feature/shop/screens/home/home.dart
+++ lib/feature/shop/screens/home/home.dart
@@ -1,7 +1,9 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:socialclone/comon/widgets/images/rounded_image.dart';
 import 'package:socialclone/comon/widgets/products/cart/cart_counter_icon.dart';
 import 'package:socialclone/comon/widgets/textfields/search_bar.dart';
 import 'package:socialclone/feature/shop/screens/home/widgets/home_appbar.dart';
@@ -10,6 +12,7 @@ import 'package:socialclone/feature/shop/screens/home/widgets/primary_header_con
 import 'package:socialclone/utils/constants/colors.dart';
 import 'package:socialclone/utils/constants/sizes.dart';
 import 'package:socialclone/utils/constants/texts.dart';
+import 'package:socialclone/utils/images.dart';
 import '../../../../comon/widgets/appbar/appbar.dart';
 import '../../../../comon/widgets/custom_shape/circular_container.dart';
 import '../../../../utils/helpers/device_helpers.dart';
@@ -20,30 +23,55 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          SizedBox(height: USizes.profilePrimaryHeaderHeight * 2),
+          ///////////////Upper Part////////////////
+          Stack(
+            children: [
+              SizedBox(height: USizes.profilePrimaryHeaderHeight * 2),
 
-          // Primary Header Container
-          UPrimaryHeaderContainer(
+              // Primary Header Container
+              UPrimaryHeaderContainer(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    UHomeAppbar(context),
+                    SizedBox(height: USizes.spaceBtwSections),
+
+                    // Home  Categories
+                    UHomeCategoires(),
+                  ],
+                ),
+              ),
+
+              // Search Bar
+              USearchBar(),
+            ],
+          ),
+          SizedBox(height: USizes.defaultSpace),
+
+          ///////////////Lower Part////////////////
+          // Banners
+          Padding(
+            padding: const EdgeInsets.all(8.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                UHomeAppbar(context),
-                SizedBox(height: USizes.spaceBtwSections),
-
-                // Home  Categories
-                UHomeCategoires(),
+                CarouselSlider(
+                  items: [
+                    URoundedImage(imageUrl: UImages.homeBanner1,),
+                    URoundedImage(imageUrl: UImages.homeBanner2,),
+                    URoundedImage(imageUrl: UImages.homeBanner3,),
+                    URoundedImage(imageUrl: UImages.homeBanner4,),
+                    URoundedImage(imageUrl: UImages.homeBanner5,),
+                    URoundedImage(imageUrl: UImages.homeBanner6,),
+                  ],
+                  options: CarouselOptions(viewportFraction: 1.0, autoPlay: true,),
+                ),
               ],
             ),
           ),
-
-          // Search Bar
-          USearchBar(),
         ],
       ),
     );
   }
 }
-
-
